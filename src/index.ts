6e953532c6fb588b48644ae3bd823fd81623c464
Spencer-0003 <spencerthe0003@gmail.com>
2021-03-03 02:47:43 +0000

    Added a few functions.

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,42 @@ import superagent from "superagent";
 
 const BASE_URL = "https://api.otakugifs.xyz/gif/";
 
-export default class OtakuGifs {
+export class OtakuGifs {
     key: string; // API key.
 
     constructor(apiKey: string) {
         this.key = apiKey;
+    };
+
+    async angrystare() {
+        let { body } = await superagent
+        .get(`${BASE_URL}/angrystare`)
+        .set("X-API-KEY", this.key);
+
+        return body.url;
+    };
+
+    async bite() {
+        let { body } = await superagent
+        .get(`${BASE_URL}/bite`)
+        .set("X-API-KEY", this.key);
+
+        return body.url;
+    };
+
+    async bleh() {
+        let { body } = await superagent
+        .get(`${BASE_URL}/bleh`)
+        .set("X-API-KEY", this.key);
+
+        return body.url;
+    };
+
+    async blush() {
+        let { body } = await superagent
+        .get(`${BASE_URL}/blush`)
+        .set("X-API-KEY", this.key);
+
+        return body.url;
     };
 };
